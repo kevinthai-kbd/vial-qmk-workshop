6fd28ff8bac5af75d2b5a9da235dbaeed82b9b9e
bevin <kevinthai177@gmail.com>
2023-04-21 12:18:46 -0700

    fix

--- keyboards/zotpad/rules.mk
+++ keyboards/zotpad/rules.mk
@@ -16,11 +16,11 @@ BOOTLOADER = caterina
 LTO_ENABLE = yes
 BOOTMAGIC_ENABLE = yes     # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = yes        # Console for debug
-COMMAND_ENABLE = yes        # Commands for debug and configuration
+EXTRAKEY_ENABLE = no       # Audio control and System control
+CONSOLE_ENABLE = no        # Console for debug
+COMMAND_ENABLE = no        # Commands for debug and configuration
 NKRO_ENABLE = yes           # USB Nkey Rollover 
-MIDI_ENABLE = yes           # MIDI support
+MIDI_ENABLE = no           # MIDI support
 UNICODE_ENABLE = yes        # Unicode
 
 COMBO_ENABLE = yes    
